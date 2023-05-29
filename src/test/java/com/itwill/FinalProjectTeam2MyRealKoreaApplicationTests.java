66d23b5d360e9b204e2dcd01d93168f289599523
kgee2 <kgee12300@gmail.com>
2023-05-29 23:04:39 +0900

    test 주석

--- src/test/java/com/itwill/FinalProjectTeam2MyRealKoreaApplicationTests.java
+++ src/test/java/com/itwill/FinalProjectTeam2MyRealKoreaApplicationTests.java
@@ -3,10 +3,10 @@ package com.itwill;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+//@SpringBootTest
 class FinalProjectTeam2MyRealKoreaApplicationTests {
 
-	@Test
+//	@Test
 	void contextLoads() {
 	}
 
